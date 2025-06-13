b807350c2c76bb9ba24631ac2888d23ad3d1e495
RakovJavaAQA <rakovaqa@yandex.ru>
2025-06-13 18:04:04 +0100

    Переделал MainPage и MainPageTest под параметризацию

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -30,42 +30,6 @@ public class MainPage {
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(pathTitleImportantQuestions));
     }
 
-    //Блок с вопросами
-    // Вопрос №1
-    private final By pathQuetion_1 = By.xpath(".//div[@id='accordion__heading-0']");
-    // Вопрос №2
-    private final By pathQuestion_2 = By.xpath(".//div[@id='accordion__heading-1']");
-    // Вопрос №3
-    private final By pathQuestion_3 = By.xpath(".//div[@id='accordion__heading-2']");
-    // Вопрос №4
-    private final By pathQuestion_4 = By.xpath(".//div[@id='accordion__heading-3']");
-    // Вопрос №5
-    private final By pathQuestion_5 = By.xpath(".//div[@id='accordion__heading-4']");
-    // Вопрос №6
-    private final By pathQuestion_6 = By.xpath(".//div[@id='accordion__heading-5']");
-    // Вопрос №7
-    private final By pathQuestion_7 = By.xpath(".//div[@id='accordion__heading-6']");
-    // Вопрос №8
-    private final By pathQuestion_8 = By.xpath(".//div[@id='accordion__heading-7']");
-
-    //Блок с ответами
-    // Ответ №1
-    private final By pathAnswer_1 = By.xpath(".//div[@id='accordion__panel-0']");
-    // Ответ №2
-    private final By pathAnswer_2 = By.xpath(".//div[@id='accordion__panel-1']");
-    // Ответ №3
-    private final By pathAnswer_3 = By.xpath(".//div[@id='accordion__panel-2']");
-    // Ответ №4
-    private final By pathAnswer_4 = By.xpath(".//div[@id='accordion__panel-3']");
-    // Ответ №5
-    private final By pathAnswer_5 = By.xpath(".//div[@id='accordion__panel-4']");
-    // Ответ №6
-    private final By pathAnswer_6 = By.xpath(".//div[@id='accordion__panel-5']");
-    // Ответ №7
-    private final By pathAnswer_7 = By.xpath(".//div[@id='accordion__panel-6']");
-    // Ответ №8
-    private final By pathAnswer_8 = By.xpath(".//div[@id='accordion__panel-7']");
-
     //Открыть URL главной страницы
     public void mainPageOpen () {
         driver.get(MAIN_PAGE_URL);
@@ -87,56 +51,17 @@ public class MainPage {
     }
 
     //Блок получения текстов ответов
-    public String textAnswer1() {
-        return driver.findElement(pathAnswer_1).getText();
+    public String textAnswer(int numberAnswer) {
+        String dynamicXpathAnswer = String.format(".//div[@id='accordion__panel-%d']", numberAnswer);
+        By pathAnswer = By.xpath(dynamicXpathAnswer);
+        return driver.findElement(pathAnswer).getText();
     }
-    public String textAnswer2() {
-        return driver.findElement(pathAnswer_2).getText();
-    }
-    public String textAnswer3() {
-        return driver.findElement(pathAnswer_3).getText();
-    }
-    public String textAnswer4() {
-        return driver.findElement(pathAnswer_4).getText();
-    }
-    public String textAnswer5() {
-        return driver.findElement(pathAnswer_5).getText();
-    }
-    public String textAnswer6() {
-        return driver.findElement(pathAnswer_6).getText();
-    }
-    public String textAnswer7() {
-        return driver.findElement(pathAnswer_7).getText();
-    }
-    public String textAnswer8() {
-        return driver.findElement(pathAnswer_8).getText();
-    }
-
 
     //Блок кликаем на текст вопросов
-    public void clickQuestion1() {
-        driver.findElement(pathQuetion_1).click();
-    }
-    public void clickQuestion2() {
-        driver.findElement(pathQuestion_2).click();
-    }
-    public void clickQuestion3() {
-        driver.findElement(pathQuestion_3).click();
-    }
-    public void clickQuestion4() {
-        driver.findElement(pathQuestion_4).click();
-    }
-    public void clickQuestion5() {
-        driver.findElement(pathQuestion_5).click();
-    }
-    public void clickQuestion6() {
-        driver.findElement(pathQuestion_6).click();
-    }
-    public void clickQuestion7() {
-        driver.findElement(pathQuestion_7).click();
-    }
-    public void clickQuestion8() {
-        driver.findElement(pathQuestion_8).click();
+    public void clickQuestion(int questionNumber) {
+        String dynamicXpathQuestion = String.format(".//div[@id='accordion__heading-%d']", questionNumber);
+        By pathQuestion = By.xpath(dynamicXpathQuestion);
+        driver.findElement(pathQuestion).click();
     }
 
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,11 +1,14 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pageobject.MainPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +24,34 @@ public class MainPageTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("faqData")
+    public void compareAnswer(String expectedResult,
+                              int questionNumber,
+                              int numberAnswer) {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.scrollPageToImportantQuestions();
+        mainPage.clickQuestion(questionNumber);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String actualResult = mainPage.textAnswer(numberAnswer);
+        assertEquals(expectedResult, actualResult, "Текст ответа не верный!");
+
+    }
+
+    private static Stream<Arguments> faqData() {
+        return Stream.of(
+                Arguments.of("Сутки — 400 рублей. Оплата курьеру — наличными или картой.", 0, 0),
+                Arguments.of("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", 1, 1),
+                Arguments.of("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", 2, 2),
+                Arguments.of("Только начиная с завтрашнего дня. Но скоро станем расторопнее.", 3, 3),
+                Arguments.of("Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", 4, 4),
+                Arguments.of("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", 5, 5),
+                Arguments.of("Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", 6, 6),
+                Arguments.of("Да, обязательно. Всем самокатов! И Москве, и Московской области.", 7, 7)
+                );
+    }
+
+    /* @Test
     public void compareAnswer1() {
         String expectedResult = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
         MainPage mainPage = new MainPage(driver);
@@ -101,7 +131,7 @@ public class MainPageTest {
         mainPage.clickQuestion8();
         String actualResult = mainPage.textAnswer8();
         assertEquals(expectedResult, actualResult, "Текст ответа не верный!");
-    }
+    } */
 
     @AfterEach
     public void cleanUp() {
